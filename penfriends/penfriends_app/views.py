ea2664fcbdac21ef0d095b005577b8ec16b2611d
markcantrell <m@rk.codes>
2021-09-18 15:11:15 -0400

    updated html, added views and urls

--- penfriends/penfriends_app/views.py
+++ penfriends/penfriends_app/views.py
@@ -1,3 +1,4 @@
+from penfriends.penfriends_app.models import Message
 from django.shortcuts import render
 
 from django.shortcuts import render, redirect, HttpResponse
@@ -13,3 +14,35 @@ def penfriends(request): #GET REQUEST
         "this_user": User.objects.get(id=request.session["user_id"]),
     }
     return render(request, "penfriends.html", context)
+
+    # mark's views
+
+def inbox(request):
+    if "user_id" not in request.session:
+        messages.error(request, "You must be logged in to view this site")
+        return redirect ("/")
+    else:
+        context = {
+            "this_user": User.objects.get(id = request.session["user_id"]),
+            "user_messages" : Message.objects.filter(recipient = request.session["user_id"]),
+        }
+        return render (request, "inbox.html", context)
+
+def message(request, message_id):
+    if "user_id" not in request.session:
+        messages.error(request, "You must be logged in to view this site")
+        return redirect ("/")
+    else:
+        context = { 
+            "this_user": User.objects.get(id = request.session["user_id"]),
+            "this_message" : Message.objects.get(id = message_id)
+        }
+        return render (request, "message.html", context)
+
+def mark_read(request, message_id):
+    this_message = Message.objects.get(id = message_id)
+    this_message.unread = False
+    this_message.save()
+    return redirect("/inbox")
+
+    # end mark's views
