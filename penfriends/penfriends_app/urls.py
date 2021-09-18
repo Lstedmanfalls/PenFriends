c5f57e39ed56cc2c0d6cf48713a199a90eac60a4
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2021-09-18 16:59:04 -0600

    corrected urls

--- penfriends/penfriends_app/urls.py
+++ penfriends/penfriends_app/urls.py
@@ -4,15 +4,15 @@ from . import views
 urlpatterns = [
     path('', views.penfriends), #GET request to display home page
     
-    # mark's urls 
+    # mark's urls - need to add beginning slash to all urls (root is penfriends links)
     
-    path('inbox', views.inbox),
-    path('inbox/', views.inbox),
-    path('message/compose', views.new_message),
-    path('message/compose/', views.new_message),
-    path('message/<int:message_id>', views.message),
-    path('message/<int:message_id>/', views.message),
-    path('message/<int:message_id>/mark_read', views.mark_read),
+    path('/inbox', views.inbox),
+    path('/inbox/', views.inbox),
+    path('/message/compose', views.new_message),
+    path('/message/compose/', views.new_message),
+    path('/message/<int:message_id>', views.message),
+    path('/message/<int:message_id>/', views.message),
+    path('/message/<int:message_id>/mark_read', views.mark_read),
 
     # end mark's urls 
 ]
